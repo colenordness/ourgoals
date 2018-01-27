ee587858f369af3de7c456301627d09d5c75ee44
Cole Nordness <colenordness@gmail.com>
2018-01-26 23:38:41 -0500

    Added Comment

--- app/src/main/java/com/example/cole/ourgoals/MainActivity.java
+++ app/src/main/java/com/example/cole/ourgoals/MainActivity.java
@@ -6,7 +6,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
-
+//Cole
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
